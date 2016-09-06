47b81241d2f34673e573a0b4e19e650354c33508
Diego Casorran <dc@mega.co.nz>
2016-09-06 18:35:52 +0200

    Adding PBKDF2 support.

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ aes-exports,aes-ecb-exports,aes-cbc-exports,aes-ctr-exports,aes-ccm-exports,\
 aes-gcm-exports,hash,sha256,sha256-exports,sha512,sha512-exports,hmac,\
 hmac-sha256,hmac-sha512,hmac-sha256-exports,hmac-sha512-exports,rng,\
 rng-exports,rng-globals,bn,bn-exports,rsa,rsa-raw,rsa-keygen-exports,\
+pbkdf2,pbkdf2-hmac-sha512,pbkdf2-hmac-sha512-exports,\
 rsa-raw-exports
 
 if [[ "$BRANCH" = "" ]]; then
